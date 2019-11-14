1da48c08b9e68edec63dcb72df787fad78d2a938
bikash <bikash.suwal01@gmail.com>
2019-11-14 21:33:42 +0545

    welcome style and welcome php changed

--- inc/welcome/welcome.php
+++ inc/welcome/welcome.php
@@ -1,19 +1,19 @@
 <?php
-	if(!class_exists('Grace_Mag_Welcome')) :
+    if(!class_exists('Grace_Mag_Welcome')) :
 
-		class Grace_Mag_Welcome {
+        class Grace_Mag_Welcome {
 
-			public $tab_sections = array(); // Welcome Page Tab Sections
-			public $theme_name = null; // For storing Theme Name
-			public $theme_slug = null; // For storing Theme Slug
-			public $theme_version = null; // For Storing Theme Current Version Information
-			public $free_plugins = array(); // Displayed Under Recommended Tabs
-			public $pro_plugins = array(); // Will be displayed under Recommended Plugins
-			public $req_plugins = array(); // Will be displayed under Required Plugins Tab
-			public $companion_plugins = array(); // Will be displayed under Demo Import Tab
-			public $strings = array(); // Common Display Strings
+            public $tab_sections = array(); // Welcome Page Tab Sections
+            public $theme_name = null; // For storing Theme Name
+            public $theme_slug = null; // For storing Theme Slug
+            public $theme_version = null; // For Storing Theme Current Version Information
+            public $free_plugins = array(); // Displayed Under Recommended Tabs
+            public $pro_plugins = array(); // Will be displayed under Recommended Plugins
+            public $req_plugins = array(); // Will be displayed under Required Plugins Tab
+            public $companion_plugins = array(); // Will be displayed under Demo Import Tab
+            public $strings = array(); // Common Display Strings
 
-			/**
+            /**
              * Get notice screenshot based on previous theme.
              *
              * @return string Image url.
@@ -25,53 +25,53 @@
                 return $main_dir . '/screenshot.png';
             }
 
-			public function __construct( $strings ) {
+            public function __construct( $strings ) {
 
-				/** Useful Variables **/
-				$theme = wp_get_theme();
-				$this->theme_name = $theme->Name;
-				$this->theme_slug = $theme->TextDomain;
-				$this->theme_version = $theme->Version;
-				$this->theme_page = esc_html( 'welcome-page' );
-				$this->plugin_install_page = esc_html( 'tgmpa-install-plugins' );
-				$this->import_demo_page = esc_html( 'everest-demo-importer' );
-				$this->doc_link = esc_url( 'https://everestthemes.com/doc/grace-mag-wordpress-theme-documentation/' );
-				$this->pro_link = esc_url( 'https://everestthemes.com/themes/grace-mag-pro/' );
-				$this->review_link = esc_url( 'https://wordpress.org/support/theme/grace-mag/reviews/#new-post' );
-				$this->support_link = esc_url( 'https://everestthemes.com/support-forum/' );
+                /** Useful Variables **/
+                $theme = wp_get_theme();
+                $this->theme_name = $theme->Name;
+                $this->theme_slug = $theme->TextDomain;
+                $this->theme_version = $theme->Version;
+                $this->theme_page = esc_html( 'welcome-page' );
+                $this->plugin_install_page = esc_html( 'tgmpa-install-plugins' );
+                $this->import_demo_page = esc_html( 'everest-demo-importer' );
+                $this->doc_link = esc_url( 'https://everestthemes.com/doc/grace-mag-wordpress-theme-documentation/' );
+                $this->pro_link = esc_url( 'https://everestthemes.com/themes/grace-mag-pro/' );
+                $this->review_link = esc_url( 'https://wordpress.org/support/theme/grace-mag/reviews/#new-post' );
+                $this->support_link = esc_url( 'https://everestthemes.com/support-forum/' );
 
-				/** Tabs **/
-				$this->tab_sections = array(
-					'getting_started' 		=> esc_html__('Getting Started', 'grace-mag'),
-					'changelogs'			=> esc_html__('Changelogs','grace-mag'),
-					'free_vs_pro'			=> esc_html__('Free Vs Pro','grace-mag'),
-					'support' 				=> esc_html__('Support', 'grace-mag'),
-				);
+                /** Tabs **/
+                $this->tab_sections = array(
+                    'getting_started'       => esc_html__('Getting Started', 'grace-mag'),
+                    'changelogs'            => esc_html__('Changelogs','grace-mag'),
+                    'free_vs_pro'           => esc_html__('Free Vs Pro','grace-mag'),
+                    'support'               => esc_html__('Support', 'grace-mag'),
+                );
 
-				/** Strings **/
-				$this->strings = $strings;
+                /** Strings **/
+                $this->strings = $strings;
 
-				/* Theme Activation Notice */
-				add_action( 'load-themes.php', array( $this, 'activation_admin_notice' ) );
+                /* Theme Activation Notice */
+                add_action( 'load-themes.php', array( $this, 'activation_admin_notice' ) );
 
-				/* Create a Welcome Page */
-				add_action( 'admin_menu', array( $this, 'welcome_register_menu' ) );
+                /* Create a Welcome Page */
+                add_action( 'admin_menu', array( $this, 'welcome_register_menu' ) );
 
-				/* Enqueue Styles & Scripts for Welcome Page */
-				add_action( 'admin_enqueue_scripts', array( $this, 'welcome_styles_and_scripts' ) );
+                /* Enqueue Styles & Scripts for Welcome Page */
+                add_action( 'admin_enqueue_scripts', array( $this, 'welcome_styles_and_scripts' ) );
 
-			}
+            }
 
-			/** Welcome Message Notification on Theme Activation **/
-			public function activation_admin_notice() {
+            /** Welcome Message Notification on Theme Activation **/
+            public function activation_admin_notice() {
 
-				global $pagenow;
+                global $pagenow;
 
-				if( is_admin() && ('themes.php' == $pagenow)  ) {
+                if( is_admin() && ('themes.php' == $pagenow)  ) {
 
-					add_action( 'admin_notices', array( $this, 'welcome_admin_notice_display' ) );
-				}
-			}
+                    add_action( 'admin_notices', array( $this, 'welcome_admin_notice_display' ) );
+                }
+            }
 
             /**
              * Render welcome notice content
@@ -163,30 +163,30 @@
                 );
             }
 
-			/** Register Menu for Welcome Page **/
-			public function welcome_register_menu() {
+            /** Register Menu for Welcome Page **/
+            public function welcome_register_menu() {
 
-				$title        = esc_html($this->strings['welcome_menu_text']);
-				add_theme_page( esc_html($this->strings['welcome_menu_text']), $title , 'edit_theme_options', 'welcome-page', array( $this, 'welcome_screen' ));
-			}
+                $title        = esc_html($this->strings['welcome_menu_text']);
+                add_theme_page( esc_html($this->strings['welcome_menu_text']), $title , 'edit_theme_options', 'welcome-page', array( $this, 'welcome_screen' ));
+            }
 
-			/** Welcome Page **/
-			public function welcome_screen() {
+            /** Welcome Page **/
+            public function welcome_screen() {
 
-				$tabs = $this->tab_sections;
+                $tabs = $this->tab_sections;
 
-				$current_section = isset($_GET['section']) ? sanitize_text_field( wp_unslash( $_GET['section'] ) ) : 'getting_started';
+                $current_section = isset($_GET['section']) ? sanitize_text_field( wp_unslash( $_GET['section'] ) ) : 'getting_started';
 
-				$section_inline_style = '';
-				?>
-				<div class="et-welcome-header">
-					<div class="inner-header-wrapp">
-					<h1><?php echo esc_html($this->theme_name); ?></h1>
-					<span><?php echo esc_html($this->theme_version); ?></span>
-					<div class="about-text"><?php echo wp_kses_post($this->strings['theme_short_description']); ?></div>
-					</div>
-				</div>
-				<div class="wrap about-wrap">
+                $section_inline_style = '';
+                ?>
+                <div class="et-welcome-header" style="">
+                    <div class="inner-header-wrapp">
+                    <h1><?php echo esc_html($this->theme_name); ?></h1>
+                    <span><?php echo esc_html($this->theme_version); ?></span>
+                    <div class="about-text"><?php echo wp_kses_post($this->strings['theme_short_description']); ?></div>
+                    </div>
+                </div>
+                <div class="wrap about-wrap">
                     <div class="nav-tab-wrapper clearfix">
                         <?php foreach($tabs as $id => $label) : ?>
                             <?php
@@ -214,55 +214,55 @@
                             <?php require_once get_template_directory() . '/inc/welcome/sections/'.esc_html($section).'.php'; ?>
                         </div>
                     </div>
-			   	</div>
-				<?php
-			}
+                </div>
+                <?php
+            }
 
-			/** Enqueue Necessary Styles and Scripts for the Welcome Page **/
-			public function welcome_styles_and_scripts() {
+            /** Enqueue Necessary Styles and Scripts for the Welcome Page **/
+            public function welcome_styles_and_scripts() {
 
-				wp_enqueue_style( 'grace-mag' . '-welcome-screen', get_template_directory_uri() . '/inc/welcome/css/welcome.css' );
-				wp_enqueue_script( 'grace-mag' . '-welcome-screen', get_template_directory_uri() . '/inc/welcome/js/welcome.js', array( 'jquery' ) );
-			}
+                wp_enqueue_style( 'grace-mag' . '-welcome-screen', get_template_directory_uri() . '/inc/welcome/css/welcome.css' );
+                wp_enqueue_script( 'grace-mag' . '-welcome-screen', get_template_directory_uri() . '/inc/welcome/js/welcome.js', array( 'jquery' ) );
+            }
 
-	      	public function admin_sidebar_contents() {
+            public function admin_sidebar_contents() {
 
                 $pro_link = $this->pro_link;
                 $doc_link = $this->doc_link;
                 $review_link = $this->review_link;
 
                 ?>
-	      		<div class="operation-admin-side-wrapper">
+                <div class="operation-admin-side-wrapper">
 
-	      			<div class="operation-side-box-wrapper operation-welcome-box-white">
-						<div class="box-header et-box-header"><?php esc_html_e('Buy Pro','grace-mag'); ?></div>
-						<div class="box-content">
-							<p><?php esc_html_e('Get advance options to customize your site and more layouts.','grace-mag'); ?></p>
-							<a href="<?php echo esc_url( $pro_link );?>" class="button button-primary" target="_blank"><?php esc_html_e('Pro Details','grace-mag'); ?></a>
-						</div>
-					</div>
+                    <div class="operation-side-box-wrapper operation-welcome-box-white">
+                        <div class="box-header et-box-header"><?php esc_html_e('Buy Pro','grace-mag'); ?></div>
+                        <div class="box-content">
+                            <p><?php esc_html_e('Get advance options to customize your site and more layouts.','grace-mag'); ?></p>
+                            <a href="<?php echo esc_url( $pro_link );?>" class="button button-primary" target="_blank"><?php esc_html_e('Pro Details','grace-mag'); ?></a>
+                        </div>
+                    </div>
 
-					<div class="operation-side-box-wrapper operation-welcome-box-white">
-						<div class="box-header et-box-header"><?php esc_html_e('Theme Documentation','grace-mag'); ?></div>
-						<div class="box-content">
-							<p><?php esc_html_e('We have online proper documentation to know every options and use them, go through the documentation to get more familiar with theme','grace-mag'); ?></p>
-							<a href="<?php echo esc_url( $doc_link );?>" class="button button-primary" target="_blank"><?php esc_html_e('Read Now','grace-mag'); ?></a>
-						</div>
-					</div>
+                    <div class="operation-side-box-wrapper operation-welcome-box-white">
+                        <div class="box-header et-box-header"><?php esc_html_e('Theme Documentation','grace-mag'); ?></div>
+                        <div class="box-content">
+                            <p><?php esc_html_e('We have online proper documentation to know every options and use them, go through the documentation to get more familiar with theme','grace-mag'); ?></p>
+                            <a href="<?php echo esc_url( $doc_link );?>" class="button button-primary" target="_blank"><?php esc_html_e('Read Now','grace-mag'); ?></a>
+                        </div>
+                    </div>
 
-					<div class="operation-side-box-wrapper operation-welcome-box-white">
-						<div class="box-header et-box-header"><?php printf(esc_html__('Loving %1$s ? ','grace-mag'),$this->theme_name); ?></div>
-						<div class="box-content">
+                    <div class="operation-side-box-wrapper operation-welcome-box-white">
+                        <div class="box-header et-box-header"><?php printf(esc_html__('Loving %1$s ? ','grace-mag'),$this->theme_name); ?></div>
+                        <div class="box-content">
                             <p><?php printf( esc_html__( 'Are you are enjoying %1$s? We would love to hear your feedback.','grace-mag' ),$this->theme_name); ?></p>
-							<a href="<?php echo esc_url( $review_link );?>" class="button button-primary" target="_blank"><?php esc_html_e('Leave Us Review','grace-mag'); ?></a>
-						</div>
-					</div>
+                            <a href="<?php echo esc_url( $review_link );?>" class="button button-primary" target="_blank"><?php esc_html_e('Leave Us Review','grace-mag'); ?></a>
+                        </div>
+                    </div>
 
-				</div>
-			<?php
-	      	}
+                </div>
+            <?php
+            }
 
-		}
+        }
 
-	endif;
+    endif;
 ?>
